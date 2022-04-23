d1d16bc04a1bf88dfe0dc4bd5d430119760602b5
thuangb <87744278+thuangb@users.noreply.github.com>
2022-04-23 14:19:39 +0700

    Simplify default path for tokenizer

--- enhancing/utils/tokenizer.py
+++ enhancing/utils/tokenizer.py
@@ -15,7 +15,7 @@ import torch
 
 @lru_cache()
 def default_bpe():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "/boosting-transformers/assets/vocab/bpe_simple_vocab_16e6.txt")
+    return 'assets/vocab/bpe_simple_vocab_16e6.txt'
 
 @lru_cache()
 def bytes_to_unicode() -> Dict:
