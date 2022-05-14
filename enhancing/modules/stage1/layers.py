0c0eab3aeedf294b2942f651f7237b3a8834d3c3
thuangb <87744278+thuangb@users.noreply.github.com>
2022-05-14 11:40:33 +0700

    Refactor code for MHSA

--- enhancing/modules/stage1/layers.py
+++ enhancing/modules/stage1/layers.py
@@ -84,12 +84,12 @@ class Attention(nn.Module):
         qkv = self.to_qkv(x).chunk(3, dim = -1)
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
 
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-
+        attn = torch.matmul(q, k.transpose(-1, -2)) * self.scale
         attn = self.attend(dots)
 
         out = torch.matmul(attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
+
         return self.to_out(out)
 
 
